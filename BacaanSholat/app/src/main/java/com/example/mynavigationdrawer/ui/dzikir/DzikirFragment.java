4735d89b741229ad247ea52516551f1ee44b1c42
MitaAnggraenii <62734174+MitaAnggraenii@users.noreply.github.com>
2021-12-03 19:00:25 +0700

    Update DzikirFragment.java

--- BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/dzikir/DzikirFragment.java
+++ BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/dzikir/DzikirFragment.java
@@ -1,8 +1,47 @@
 package com.example.mynavigationdrawer.ui.dzikir;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mynavigationdrawer.R;
+import com.example.mynavigationdrawer.databinding.FragmentDzikirBinding;
+import com.example.mynavigationdrawer.databinding.FragmentHomeBinding;
 
 
 public class DzikirFragment extends Fragment {
+    private DzikirViewModel dzikirViewModel;
+    private FragmentDzikirBinding binding;
 
-    
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dzikirViewModel =
+                new ViewModelProvider(this).get(DzikirViewModel.class);
+
+        binding = FragmentDzikirBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final ScrollView textView = binding.textDzikir;
+        dzikirViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setTextDirection(Integer.parseInt(s));
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
