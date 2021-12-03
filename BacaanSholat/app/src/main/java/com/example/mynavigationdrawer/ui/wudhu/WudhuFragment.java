bea690923a22ebb0fa960bf1290573505e40b516
Viny Silvia <62374456+VinySilvia@users.noreply.github.com>
2021-12-02 23:54:57 -1200

    Update WudhuFragment.java

--- BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/wudhu/WudhuFragment.java
+++ BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/wudhu/WudhuFragment.java
@@ -1,4 +1,11 @@
+package com.example.mynavigationdrawer.ui.wudhu;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
