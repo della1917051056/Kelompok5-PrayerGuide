e71cdb76194bcd956a6f9ace9427400ff034e392
MitaAnggraenii <62734174+MitaAnggraenii@users.noreply.github.com>
2021-12-03 19:02:16 +0700

    Update DzikirViewModel.java

--- BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/dzikir/DzikirViewModel.java
+++ BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/dzikir/DzikirViewModel.java
@@ -1,6 +1,20 @@
 package com.example.mynavigationdrawer.ui.dzikir;
 
-public class DzikirViewModel extends ViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
-   
+import com.example.mynavigationdrawer.R;
+
+public class DzikirViewModel extends ViewModel {
+   private MutableLiveData<String> mText;
+
+    public DzikirViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue(String.valueOf(R.layout.fragment_dzikir));
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
