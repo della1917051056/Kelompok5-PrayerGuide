a7c9243015c98042dbcb96f157a3fb9d8d178d76
Viny Silvia <62374456+VinySilvia@users.noreply.github.com>
2021-12-12 20:03:14 -1200

    Add files via upload

---
+++ BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/sunnah/MoveQashar.java
@@ -0,0 +1,16 @@
+package com.example.mynavigationdrawer.ui.sunnah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynavigationdrawer.R;
+
+public class MoveQashar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_qashar);
+    }
+}
---
+++ BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/sunnah/MoveQmaghrib.java
@@ -0,0 +1,16 @@
+package com.example.mynavigationdrawer.ui.sunnah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynavigationdrawer.R;
+
+public class MoveQmaghrib extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_qmaghrib);
+    }
+}
