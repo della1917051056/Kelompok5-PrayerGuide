0a95a6530dad8e75f77528c6f0cc5e75280ba6cb
Viny Silvia <62374456+VinySilvia@users.noreply.github.com>
2021-12-12 20:05:17 -1200

    Add files via upload

---
+++ BacaanSholat/app/src/main/java/com/example/mynavigationdrawer/ui/wajib/MaghribActivity.java
@@ -0,0 +1,16 @@
+package com.example.mynavigationdrawer.ui.wajib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynavigationdrawer.R;
+
+public class MaghribActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maghrib);
+    }
+}
